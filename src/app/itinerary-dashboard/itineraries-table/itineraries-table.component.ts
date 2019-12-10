d9f51630187f878f791e9acc195ec59bae9f09dd
JustinCast <justincastillovalladares@gmail.com>
2019-12-09 19:36:40 -0600

    feat: last changes in itineraries tables

--- src/app/itinerary-dashboard/itineraries-table/itineraries-table.component.ts
+++ src/app/itinerary-dashboard/itineraries-table/itineraries-table.component.ts
@@ -2,21 +2,20 @@ import { Component, OnInit } from '@angular/core';
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  status: boolean;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Arenal Lake Premium Package', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Gastronomic Adventure La Fortuna', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'In love with the Mountain', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Sports in La Fortuna, Why Not?', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Souvenirs Journey', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Typical Foods taste time', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Get Close to the Fauna', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'VIP Relaxing Package', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Thermal Waters Experience', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Going to the Party Package', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Arenal Lake Premium Package', status: true},
+  {position: 2, name: 'Gastronomic Adventure La Fortuna', status: true},
+  {position: 3, name: 'In love with the Mountain', status: true},
+  {position: 4, name: 'Sports in La Fortuna, Why Not?', status: true},
+  {position: 5, name: 'Souvenirs Journey', status: true},
+  {position: 6, name: 'Typical Foods taste time', status: true},
+  {position: 7, name: 'Get Close to the Fauna', status: true},
+  {position: 8, name: 'VIP Relaxing Package', status: true},
+  {position: 9, name: 'Thermal Waters Experience', status: true},
+  {position: 10, name: 'Going to the Party Package', status: true},
 ];
 
 @Component({
@@ -25,7 +24,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./itineraries-table.component.scss']
 })
 export class ItinerariesTableComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'actions'];
   dataSource = ELEMENT_DATA;
   constructor() { }
 
