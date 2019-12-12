cbf88fcd1bf68248129449ca1425892d6acf7c57
JustinCast <justincastillovalladares@gmail.com>
2019-12-12 12:58:44 -0600

    minimal changes

--- src/app/itinerary/components/itinerary-dashboard/itinerary-details/itinerary-details.component.ts
+++ src/app/itinerary/components/itinerary-dashboard/itinerary-details/itinerary-details.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./itinerary-details.component.scss']
 })
 export class ItineraryDetailsComponent implements OnInit {
-
+  images = [
+    "https://www.solidbackgrounds.com/images/1920x1080/1920x1080-mint-solid-color-background.jpg",
+    "https://www.solidbackgrounds.com/images/1920x1080/1920x1080-mint-green-solid-color-background.jpg",
+    "https://www.solidbackgrounds.com/images/1366x768/1366x768-navy-purple-solid-color-background.jpg"
+  ]
   constructor() { }
 
   ngOnInit() {
