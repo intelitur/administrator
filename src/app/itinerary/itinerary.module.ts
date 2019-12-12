4af6d4d5ce141ab06f930db0ba535037d1726b62
JustinCast <justincastillovalladares@gmail.com>
2019-12-12 11:07:17 -0600

    refactor: imported routes fixed

--- src/app/itinerary/itinerary.module.ts
+++ src/app/itinerary/itinerary.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SharedModule } from "../shared.module";
 import { CarouselComponent } from "./components/carousel/carousel.component";
-import { ItinerariesTableComponent } from './components/itineraries/itineraries-table/itineraries-table.component';
-import { ItineraryFormDialogComponent } from './components/itineraries/itinerary-form-dialog/itinerary-form-dialog.component';
-import { UploadImagesComponent } from './components/itineraries/itinerary-form-dialog/upload-images/upload-images.component';
-import { ItinerariesComponent } from './components/itineraries/itineraries.component';
 import { ItineraryDashboardComponent } from './components/itinerary-dashboard/itinerary-dashboard.component';
-import { ItineraryRootComponent } from './components/itinerary-root.component';
 import { ItineraryRoutingModule } from './itinerary-routing.module';
+import { ItineraryFormDialogComponent } from './components/itineraries/itinerary-form-dialog/itinerary-form-dialog.component';
+import { ItinerariesComponent } from './components/itineraries/itineraries.component';
+import { ItineraryRootComponent } from './components/itinerary-root.component';
 import { ItineraryDistributionComponent } from './components/itinerary-dashboard/itinerary-distribution/itinerary-distribution.component';
+import { ItinerariesTableComponent } from './components/itineraries/itineraries-table/itineraries-table.component';
+import { UploadImagesComponent } from './components/itineraries/itinerary-form-dialog/upload-images/upload-images.component';
+
 
 
 @NgModule({
