9f2e154df958c8e6b70406c3591850e8029e9b51
Dagoberto Solis <dagosolis10@gmail.com>
2019-12-24 10:29:56 -0600

    feat: add admi user, modify profile

--- src/app/users/components/profile/profile.component.ts
+++ src/app/users/components/profile/profile.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CommonService } from 'src/app/general-services/common.service';
+import { HttpErrorResponse } from "@angular/common/http";
+import { Subscription } from 'rxjs';
+import { UserProfileService } from '../../services/user-profile.service';
+import { User } from '../../models/User.class';
 
 
 @Component({
@@ -9,38 +13,65 @@ import { CommonService } from 'src/app/general-services/common.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
+  user: User;
   editProfileForm: FormGroup;
   hide = true;
   icon = "done";
   //icon = "warning";
+  private subscription: Subscription;
+  loading: boolean = false;
+  private subscriptionUserProfile: Subscription;
+  public userLoading: boolean = false;
+  public addedUserLoading: boolean = false;
 
   constructor(
     private _fb: FormBuilder,
-    public commonService: CommonService
-  ) {
-    this.editProfileForm = this._fb.group({
-      name: [this.user.name, Validators.required],
-      email: [this.user.email, Validators.required],
-      password: [this.user.password, Validators.required]
-    });
-   }
+    public commonService: CommonService,
+    public userProfileService: UserProfileService
+  ) {   }
+
+   
 
   ngOnInit() {
+
+    this.subscriptionUserProfile = this.userProfileService.getUser(15).subscribe({
+      next: (data : any) => {
+        this.user = data.data[0];
+        this.subscriptionUserProfile.unsubscribe();
+        this.loadUser();
+      }, error: (err : HttpErrorResponse)  => this.commonService.openSnackBar(`Error: ${err}`,"OK")
+    });
+  }
+
+  loadUser(){
+    this.editProfileForm = this._fb.group({
+      name: [this.user.info.name, Validators.required],
+      lastName: [this.user.info.lastName, Validators.required],
+      email: [this.user.info.email, Validators.required],
+      password: ['']
+    });
     this.editProfileForm.valueChanges.subscribe(() => {
       if (this.editProfileForm.invalid == false) this.icon = "done";
       else this.icon = "warning";
-    });
-  }
-
-  user = {
-    name: "José Pablo Brenes Alfaro",
-    email: "pablo@gmail.com",
-    password: "pablo123"
+    }); 
   }
 
   saveChanges(){
-    this.commonService.openSnackBar("Se han guardado los cambios correctamente","OK");
+    console.log('object');
+    let passChanged = false;
+    if(!this.editProfileForm.get('password').value){
+      this.user.info.password = this.editProfileForm.get('password').value;
+      passChanged = true;
+    }
+    this.user.info.name = this.editProfileForm.get('name').value;
+    this.user.info.lastName = this.editProfileForm.get('lastName').value;
+    this.user.info.email = this.editProfileForm.get('email').value;
+    this.subscriptionUserProfile = this.userProfileService.updateUser(this.user.user_id, this.user.info, passChanged).subscribe({
+      next: (data : any) => {
+        this.commonService.openSnackBar(`Se ha actualizado el usuario  ${this.user.info.name}`, "OK");
+        this.subscriptionUserProfile.unsubscribe();
+      }, error: (err : HttpErrorResponse)  => this.commonService.openSnackBar(`Error: ${err}`,"OK")
+    });
   }
 
 }
