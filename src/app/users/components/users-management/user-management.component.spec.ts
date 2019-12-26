5e73fa3589a5c6e5510c9c6c533d61cdef01beac
JustinCast <justincastillovalladares@gmail.com>
2019-12-26 13:32:22 -0600

    feat: last changes

--- src/app/users/components/users-management/user-management.component.spec.ts
+++ src/app/users/components/users-management/user-management.component.spec.ts
@@ -37,7 +37,7 @@ describe("User-Management", () => {
       available: true,
       state: false
     }
-    service.changeAvailableOrStateUser(18,testInfo).subscribe({
+    service.changeAvailableOrStateUser(18, testInfo as any).subscribe({
       next: (data: any) => {
 
         expect(data.code).toBe(200);
@@ -66,7 +66,7 @@ describe("User-Management", () => {
       available: false,
       state: false
     }
-    service.changeAvailableOrStateUser(18,testInfo).subscribe({
+    service.changeAvailableOrStateUser(18, testInfo as any).subscribe({
       next: (data: any) => {
 
         expect(data.code).toBe(200);
@@ -94,7 +94,7 @@ describe("User-Management", () => {
       available: false,
       state: true
     }
-    service.changeAvailableOrStateUser(18,testInfo).subscribe({
+    service.changeAvailableOrStateUser(18, testInfo as any).subscribe({
       next: (data: any) => {
 
         expect(data.code).toBe(200);
