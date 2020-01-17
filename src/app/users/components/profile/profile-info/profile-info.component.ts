21df972b17ef635b8d2ae50e6182d5183069e520
JustinCast <justincastillovalladares@gmail.com>
2020-01-17 09:20:04 -0600

    feat: minimal changes

--- src/app/users/components/profile/profile-info/profile-info.component.ts
+++ src/app/users/components/profile/profile-info/profile-info.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { CommonService } from 'src/app/general-services/common.service';
-import { UserService } from 'src/app/users/services/user.service';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Subscription } from "rxjs";
+import { CommonService } from "src/app/general-services/common.service";
+import { UserService } from "src/app/users/services/user.service";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
-  selector: 'app-profile-info',
-  templateUrl: './profile-info.component.html',
-  styleUrls: ['./profile-info.component.scss']
+  selector: "app-profile-info",
+  templateUrl: "./profile-info.component.html",
+  styleUrls: ["./profile-info.component.scss"]
 })
 export class ProfileInfoComponent implements OnInit {
   user: any;
@@ -25,24 +25,28 @@ export class ProfileInfoComponent implements OnInit {
   constructor(
     private _fb: FormBuilder,
     public commonService: CommonService,
-    public userService: UserService,
-  ) { }
+    public userService: UserService
+  ) {}
 
   ngOnInit() {
-    this.subscriptionUserProfile = this.userService.getUser(this.userService.actualUser.user_id).subscribe({
-      next: (data : any) => {
-        this.user = data.data[0];
-        this.subscriptionUserProfile.unsubscribe();
-        this.loadUser();
-      }, error: (err : HttpErrorResponse)  => this.commonService.openSnackBar(`Error: ${err}`,"OK")
-    });
+    this.subscriptionUserProfile = this.userService
+      .getUser(this.userService.actualUser.user_id)
+      .subscribe({
+        next: (data: any) => {
+          this.user = data.data[0];
+          this.subscriptionUserProfile.unsubscribe();
+          this.loadUser();
+        },
+        error: (err: HttpErrorResponse) =>
+          this.commonService.openSnackBar(`Error: ${err}`, "OK")
+      });
   }
-  loadUser(){
+  loadUser() {
     this.editProfileForm = this._fb.group({
       name: [this.user.info.name, Validators.required],
       lastName: [this.user.info.lastName, Validators.required],
-      email: [this.user.info.email, Validators.required],
-      password: ['']
+      email: [this.user.info.email, Validators.email],
+      password: [""]
     });
     this.editProfileForm.valueChanges.subscribe(() => {
       if (this.editProfileForm.invalid == false) this.icon = "done";
@@ -50,21 +54,27 @@ export class ProfileInfoComponent implements OnInit {
     });
   }
 
-  saveChanges(){
+  saveChanges() {
     let passChanged = false;
-    if(this.editProfileForm.get('password').value != ''){
-      this.user.info.password = this.editProfileForm.get('password').value;
+    if (this.editProfileForm.get("password").value != "") {
+      this.user.info.password = this.editProfileForm.get("password").value;
       passChanged = true;
     }
-    this.user.info.name = this.editProfileForm.get('name').value;
-    this.user.info.lastName = this.editProfileForm.get('lastName').value;
-    this.user.info.email = this.editProfileForm.get('email').value;
-    this.subscriptionUserProfile = this.userService.updateUser(this.user.user_id, this.user.info, passChanged).subscribe({
-      next: (data : any) => {
-        this.commonService.openSnackBar(`Se ha actualizado el usuario  ${this.user.info.name}`, "OK");
-        this.subscriptionUserProfile.unsubscribe();
-      }, error: (err : HttpErrorResponse)  => this.commonService.openSnackBar(`Error: ${err}`,"OK")
-    });
+    this.user.info.name = this.editProfileForm.get("name").value;
+    this.user.info.lastName = this.editProfileForm.get("lastName").value;
+    this.user.info.email = this.editProfileForm.get("email").value;
+    this.subscriptionUserProfile = this.userService
+      .updateUser(this.user.user_id, this.user.info, passChanged)
+      .subscribe({
+        next: (data: any) => {
+          this.commonService.openSnackBar(
+            `Se ha actualizado el usuario  ${this.user.info.name}`,
+            "OK"
+          );
+          this.subscriptionUserProfile.unsubscribe();
+        },
+        error: (err: HttpErrorResponse) =>
+          this.commonService.openSnackBar(`Error: ${err}`, "OK")
+      });
   }
-
 }
