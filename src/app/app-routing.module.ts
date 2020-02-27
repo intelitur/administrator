297ee25091e78026cb4e0bc543dd25b78777700e
Jefferson Torres <jefersontorres000@gmail.com>
2020-02-27 07:37:35 -0600

    error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoggedInGuard } from './logged-in.guard';
 const routes: Routes = [
-  {
+  { 
     path: "itinerary",
     loadChildren: () =>
       import("./itinerary/itinerary.module").then(
