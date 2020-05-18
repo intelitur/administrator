d3b6e93ac80ddcdb2958411b6f5ac7d9806bad47
Walter Benavides <wbenafi@gmail.com>
2020-05-18 11:21:58 -0600

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  localstorage_key: 'key_user',
+  SERVER_BASE_URL: 'https:intelitur.sytes.net/',
+  IMAGES_URL_BASE: 'http://localhost:3000/static/images/'
 };
