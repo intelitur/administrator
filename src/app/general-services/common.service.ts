40d35f0e6448b1c6680d39005884be6357cbe2fc
Walter Benavides <wbenafi@gmail.com>
2020-10-20 10:55:54 -0600

    URL to upload images

--- src/app/general-services/common.service.ts
+++ src/app/general-services/common.service.ts
@@ -63,7 +63,7 @@ export class CommonService {
         }
       });
   
-      xhr.open("POST", "https://intelitur.sytes.net/adminfiles");
+      xhr.open("POST", `${environment.SERVER_BASE_URL}/adminfiles`);
   
       xhr.send(data);
       
