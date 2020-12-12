62e22c82cfe97223dd1cb004451b7b89e8bbc56f
Jose Ocon <josebermudez587@gmail.com>
2020-12-12 09:54:57 -0600

    Cambio en qrs y creacion de capas en proceso

--- src/app/geographic-layers/components/create-geographic-layer/create-geographic-layer.component.ts
+++ src/app/geographic-layers/components/create-geographic-layer/create-geographic-layer.component.ts
@@ -1,6 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from "@angular/core";
 import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { MatDialogRef } from "@angular/material";
+import { Router } from '@angular/router';
+import { CommonService } from 'src/app/general-services/common.service';
+import { GeographicLayersService } from '../../services/geographic-layers.service';
 
 @Component({
   selector: "app-create-geographic-layer",
@@ -9,10 +13,14 @@ import { MatDialogRef } from "@angular/material";
 })
 export class CreateGeographicLayerComponent implements OnInit {
   layerFG: FormGroup;
+  loading: boolean = false;
 
   constructor(
     public dialogRef: MatDialogRef<CreateGeographicLayerComponent>,
-    public fb: FormBuilder
+    public fb: FormBuilder,
+    public geographicLayerService: GeographicLayersService,
+    public commonService: CommonService,
+    private router: Router
   ) {
     this.layerFG = this.fb.group({
       name: ["", Validators.required],
@@ -53,6 +61,61 @@ export class CreateGeographicLayerComponent implements OnInit {
 
   deleteIntAtribute(index) {
     this.integerAtributes.removeAt(index);
+  }
+
+
+  submit(){ //TODO: Preguntar sobre la lat y lng
+    this.layerFG.disable();
+    this.loading = true;
+
+    let layer:any = {
+      layer_name: this.layerFG.controls['name'].value,
+      layer_detail: this.layerFG.controls['detail'].value
+    }
+
+    for (let i = 0; i < this.txtAtributes.value.length; i++) {
+      const element = this.txtAtributes.value[i].textAtribute;
+      layer[element] = {
+        name: element,
+        tipo: "string"
+      }
+    }
+
+    for (let i = 0; i < this.integerAtributes.value.length; i++) {
+      const element = this.integerAtributes.value[i].intAtribute;
+      layer[element] = {
+        name: element,
+        tipo: "integer"
+      }
+    }
+    
+    this.addLayer(layer)
+  }
+
+
+  addLayer(layer: any){
+    console.log(layer)
+    this.geographicLayerService.createLayer(layer).subscribe({
+      next: (data: any) =>{
+        if(data.status == 201){
+          console.log(data)
+          this.commonService.openSnackBar(
+            `La capa ${this.layerFG.value.name} se ha creado!`,
+            "OK"
+          );
+          this.dialogRef.close();
+          this.router.navigate([`/geographic-layers/${data.body[0]}`])
+        }else{
+          this.commonService.openSnackBar(`Ha ocurrido un error al intentar crear la capa: ${data.error}`, "OK")
+          this.layerFG.enable();
+          this.loading = true;
+        }
+      }, error: (err: HttpErrorResponse) => {
+        this.commonService.openSnackBar(`Error: ${err.message}`, "OK")
+        this.layerFG.enable();
+        this.loading = true;
+      }
+    })
   }
 
   disableDialog(): boolean {
--- src/app/geographic-layers/components/management-geographic-layer/details-geographic-layer/details-geographic-layer.component.ts
+++ src/app/geographic-layers/components/management-geographic-layer/details-geographic-layer/details-geographic-layer.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from "@angular/common/http";
+import { Component, Input, OnInit } from "@angular/core";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Subscription } from "rxjs";
+import { CommonService } from "src/app/general-services/common.service";
+import { GeographicLayersService } from "src/app/geographic-layers/services/geographic-layers.service";
 
 @Component({
-  selector: 'app-details-geographic-layer',
-  templateUrl: './details-geographic-layer.component.html',
-  styleUrls: ['./details-geographic-layer.component.scss']
+  selector: "app-details-geographic-layer",
+  templateUrl: "./details-geographic-layer.component.html",
+  styleUrls: ["./details-geographic-layer.component.scss"],
 })
 export class DetailsGeographicLayerComponent implements OnInit {
+  @Input() myLayer: any;
+  layerFG: FormGroup;
+  loading: boolean = false;
+  subscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public fb: FormBuilder,
+    public geographicLayerService: GeographicLayersService,
+    public commonService: CommonService
+  ) {
+    this.layerFG = this.fb.group({
+      name: ["", Validators.required],
+      detail: ["", Validators.required],
+      textAtributes: this.fb.array([this.fb.group({})], Validators.required),
+      intAtributes: this.fb.array([this.fb.group({})], Validators.required),
+    });
   }
 
+  ngOnInit() {}
+
+  get txtAtributes() {
+    return this.layerFG.get("textAtributes") as FormArray;
+  }
+
+  addTextAtribute() {
+    this.txtAtributes.push(
+      this.fb.group({ textAtribute: ["", Validators.required] })
+    );
+  }
+
+  deleteTextAtribute(index) {
+    this.txtAtributes.removeAt(index);
+  }
+
+  get integerAtributes() {
+    return this.layerFG.get("intAtributes") as FormArray;
+  }
+
+  addIntAtribute() {
+    this.integerAtributes.push(
+      this.fb.group({ intAtribute: ["", Validators.required] })
+    );
+  }
+
+  deleteIntAtribute(index) {
+    this.integerAtributes.removeAt(index);
+  }
+
+  setData() {
+    this.layerFG.controls["name"].setValue(this.myLayer.layer_name);
+    this.layerFG.controls["detail"].setValue(this.myLayer.detail);
+    for (let i = 0; i < this.myLayer.length; i++) {
+      const element = this.myLayer[i];
+    }
+  }
+
+  disableDialog(): boolean {
+    if (!this.layerFG.valid) {
+      return true;
+    }
+    return false;
+  }
 }
